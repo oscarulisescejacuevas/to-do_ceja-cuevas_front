585a4d9dcdfd853febadcc0b6026329ef94fb4bc
Oscar Ulises Ceja Cuevas <21222100672@soy.utj.edu.mx>
2025-12-04 18:36:21 -0600

    ojkfojafdawhjhgfdfgjgjdod

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.iiqviqc80no"
+    "revision": "0.cmd4tsp6618"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
